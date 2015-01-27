3c22bc6bf908a7d7496b576c475cf0a7921504fc
Stefano Ballerini <ballerinistefano@gmail.com>
2015-01-27 21:02:26 +0100

    QuickSortAdded

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import Algorithms.BoubleSort;
 import Algorithms.CountingSort;
 import Algorithms.InsertionSort;
 import Algorithms.MergeSort;
+import Algorithms.QuickSort;
 import Common.UtilitiesFunctions;
 
 
@@ -14,30 +15,32 @@ public class Main {
 		Random random = new Random();
 		
 		int size = 20;
-		int buffer[] = new int[size];
+		int arrayr[] = new int[size];
 		
 		int maxElement = Integer.MIN_VALUE;
 		
 
-		for (int i = 0; i < buffer.length; i++) {
-			buffer[i] = random.nextInt(100);
-			if(buffer[i] > maxElement) maxElement = buffer[i];
+		for (int i = 0; i < arrayr.length; i++) {
+			arrayr[i] = random.nextInt(100);
+			if(arrayr[i] > maxElement) maxElement = arrayr[i];
 		}	
 		
 		
-		int[] sortedArrayInsertionSort = InsertionSort.sort(UtilitiesFunctions.copy(buffer));
-		int[] sortedArrayMergeSort = MergeSort.sort(UtilitiesFunctions.copy(buffer));
-		int[] sortedArrayBoubleSort = BoubleSort.sort(UtilitiesFunctions.copy(buffer));
-		int[] sortedArraySequentialSort = BoubleSort.sort(UtilitiesFunctions.copy(buffer));
-		int[] sortedArrayCountingSort = CountingSort.sort(UtilitiesFunctions.copy(buffer), maxElement);
+		int[] sortedArrayInsertionSort = InsertionSort.sort(UtilitiesFunctions.copy(arrayr));
+		int[] sortedArrayMergeSort = MergeSort.sort(UtilitiesFunctions.copy(arrayr));
+		int[] sortedArrayBoubleSort = BoubleSort.sort(UtilitiesFunctions.copy(arrayr));
+		int[] sortedArraySequentialSort = BoubleSort.sort(UtilitiesFunctions.copy(arrayr));
+		int[] sortedArrayCountingSort = CountingSort.sort(UtilitiesFunctions.copy(arrayr), maxElement);
+		int[] sortedArrayQuickSort = QuickSort.sort(UtilitiesFunctions.copy(arrayr));
 		
 	
-		UtilitiesFunctions.printArray("Buffer", buffer);
+		UtilitiesFunctions.printArray("Array", arrayr);
 		UtilitiesFunctions.printArray("InsertionSort", sortedArrayInsertionSort);
 		UtilitiesFunctions.printArray("MergeSort", sortedArrayMergeSort);
 		UtilitiesFunctions.printArray("BoubleSort", sortedArrayBoubleSort);
 		UtilitiesFunctions.printArray("SequentialSort", sortedArraySequentialSort);
 		UtilitiesFunctions.printArray("CountingSort", sortedArrayCountingSort);
+		UtilitiesFunctions.printArray("QuickSort", sortedArrayQuickSort);
 
 		
 	}
